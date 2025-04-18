dd82805f468283074cbf7e7daed76fa60f56c94e
Dark hacker 😈 <javieldev@gmail.com>
2025-04-18 21:32:07 +0300

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,3 +1,7 @@
+//DO MODIFY THIS FILE ⚠️
+//CREATED BY DARK TECH
+//CONTACT 2547107065646
+
 const { zokou } = require('../framework/zokou');
 const { addOrUpdateDataInAlive, getDataFromAlive } = require('../bdd/alive');
 const moment = require("moment-timezone");
